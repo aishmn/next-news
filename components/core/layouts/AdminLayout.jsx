bd9347cd4ea3e12028b2dbfe46757672c28ced06
aishmn <manish.sinuwari@gmail.com>
2020-06-20 11:47:08 +0545

    updated login

--- components/core/layouts/AdminLayout.jsx
+++ components/core/layouts/AdminLayout.jsx
@@ -1,54 +1,60 @@
-import { Row } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
-import AdminSidebar from '../../admin/admin-sidebar/AdminSidebar';
-import AdminHeader from '../../admin/admin-header/AdminHeader';
-import { useDispatch, useSelector } from 'react-redux';
-import { loadUser } from '../../../redux/actions/authActions';
-import Alert from '../../alert/Alert';
-import { setNotification } from '../../../redux/actions/notificationActions';
-import setAuthToken from '../../../lib/setAuthToken';
-import Router from 'next/router';
+import { Row } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import AdminSidebar from "../../admin/admin-sidebar/AdminSidebar";
+import AdminHeader from "../../admin/admin-header/AdminHeader";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUser } from "../../../redux/actions/authActions";
+import Alert from "../../alert/Alert";
+import { setNotification } from "../../../redux/actions/notificationActions";
+import setAuthToken from "../../../lib/setAuthToken";
+import Router from "next/router";
 const AdminLayout = ({ children }) => {
-	const [showDrawer, setShowDrawer] = useState(false);
-	const clientSide = typeof window !== 'undefined';
-	const is_authenticated = useSelector((state) => state.auth.is_authenticated);
-	const loading = useSelector((state) => state.auth.loading);
-	const user = useSelector((state) => state.auth.user);
+  const [showDrawer, setShowDrawer] = useState(false);
+  const clientSide = typeof window !== "undefined";
+  const is_authenticated = useSelector((state) => state.auth.is_authenticated);
+  const loading = useSelector((state) => state.auth.loading);
+  const user = useSelector((state) => state.auth.user);
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		if (clientSide) {
-			setAuthToken(localStorage.getItem('token'));
-			if (!user) dispatch(loadUser());
-		}
-	}, [loadUser, loading, user]);
+  useEffect(() => {
+    if (clientSide) {
+      setAuthToken(localStorage.getItem("token"));
+      if (!user) dispatch(loadUser());
+    }
+  }, [loadUser, loading, user]);
 
-	if (clientSide && !user) dispatch(loadUser());
+  if (clientSide && !user) dispatch(loadUser());
 
-	if (loading) return <div>Spinner</div>;
+  if (clientSide && !loading && !user && is_authenticated === false)
+    Router.replace("/auth/signin");
 
-	if (clientSide && !loading && user === null && is_authenticated === false) Router.replace('/auth/signin');
+  if (user && user.role == "user") {
+    dispatch(
+      setNotification(
+        "You are not authorized to access admin area, reported to the admin"
+      )
+    );
+    Router.replace("/");
+  }
 
-	if (user && user.role == 'user') {
-		dispatch(setNotification('You are not authorized to access admin area, reported to the admin'));
-		Router.replace('/');
-	}
-
-	return (
-		<>
-			{!loading && user && user.role !== 'user' && (
-				<Row>
-					<Alert />
-					{showDrawer && <AdminSidebar />}
-					<div className="col m-0 shadow ">
-						<AdminHeader setShowDrawer={setShowDrawer} showDrawer={showDrawer} />
-						<div className="mx-md-5 mx-4">{children}</div>
-					</div>
-				</Row>
-			)}
-		</>
-	);
+  return (
+    <>
+      {!loading && user && user.role !== "user" && (
+        <Row>
+          <Alert />
+          {showDrawer && <AdminSidebar />}
+          <div className="col m-0 shadow ">
+            <AdminHeader
+              setShowDrawer={setShowDrawer}
+              showDrawer={showDrawer}
+            />
+            <div className="mx-md-5 mx-4">{children}</div>
+          </div>
+        </Row>
+      )}
+    </>
+  );
 };
 
 export default AdminLayout;
